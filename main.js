d2cceb8a6e3700aa8f19e7fc163cee3861342861
Filip Grdeń <riplay3@gmail.com>
2019-03-28 15:50:54 +0100

    push

--- main.js
+++ main.js
@@ -1,146 +1,149 @@
-//HTML elements
-const mainUL = document.querySelectorAll('header .about h1 ul');
-const ulHeight = document.querySelector('header .about h1 ul').offsetHeight;
-const h1 = document.querySelector('header .about h1');
-const paragraph = document.querySelector('header p');
-const aboutDiv = document.querySelector('header .about');
+$(document).ready(function () {
 
-const txt = 'Cześć, nazywam się Filip Grdeń jestem początkującym developerem oraz studentem informatyki i ekonometrii ze specjalnością "programowanie aplikacji mobilnych i webowych". Front-endem zainteresowałem się w marcu 2018 roku, a już miesiąć później uczestniczyłem w bootcampie Akademii108 w Krakowie. Programuję, ponieważ lubię tworzyć coś z niczego, dlatego też moim celem jest ciągły rozwój w technologiach webowych, aby granicą jakośći moich projektów były nie umiejętności lecz tylko wyobraźnia. Obecnie rozwijam się  w Reactie oraz w C#. ';
+    //HTML elements
+    const mainUL = document.querySelectorAll('header .about h1 ul');
+    const ulHeight = document.querySelector('header .about h1 ul').offsetHeight;
+    const h1 = document.querySelector('header .about h1');
+    const paragraph = document.querySelector('header p');
+    const aboutDiv = document.querySelector('header .about');
 
-
-// Parametry
-let indexText = 0;
-const time = 40;
-let indexTyping;
-
-// Implementacja
-const addLetter = () => {
-    if(paragraph.textContent.length == txt.length) return;
-    paragraph.textContent += txt[indexText];
-    indexText++;
-    if (indexText === txt.length) clearInterval(indexTyping);
-    if(aboutDiv.className.includes("disapear")) return;
-}
-
-const causeAddLetter = () => {
-    indexTyping = setInterval(addLetter, time);
-}
+    const txt = 'Cześć, nazywam się Filip Grdeń jestem początkującym developerem oraz studentem informatyki i ekonometrii ze specjalnością "programowanie aplikacji mobilnych i webowych". Front-endem zainteresowałem się w marcu 2018 roku, a już miesiąć później uczestniczyłem w bootcampie Akademii108 w Krakowie. Programuję, ponieważ lubię tworzyć coś z niczego, dlatego też moim celem jest ciągły rozwój w technologiach webowych, aby granicą jakośći moich projektów były nie umiejętności lecz tylko wyobraźnia. Obecnie rozwijam się  w Reactie oraz w C#. ';
 
 
-const start = () => {
-    h1.style.height = (ulHeight / 2) + 'px';
-}
+    // Parametry
+    let indexText = 0;
+    const time = 40;
+    let indexTyping;
 
-start();
-
-$('a.about').on('click', function () {
-    if(paragraph.className.includes('disapear')) {
-        $('header p').removeClass('disapear');
-        paragraph.textContent = "";
-        indexText = 0;
-        clearInterval(indexTyping);
+    // Implementacja
+    const addLetter = () => {
+        if (paragraph.textContent.length == txt.length) return;
+        paragraph.textContent += txt[indexText];
+        indexText++;
+        if (indexText === txt.length) clearInterval(indexTyping);
+        if (aboutDiv.className.includes("disapear")) return;
     }
-    $('header .about').addClass('disapear');
-    causeAddLetter();
-})
 
-$('nav a.main').on('click', function () {
-    if(aboutDiv.className.includes('disapear')) {
-        $('header .about').removeClass('disapear');
-        $('header p').addClass('disapear');
+    const causeAddLetter = () => {
+        indexTyping = setInterval(addLetter, time);
     }
-})
 
 
-$('a:not([data-type="about"])').on('click', function () {
-    const goToSection = "#" + $(this).attr('class');
-    $('body, html').animate({
-        scrollTop: $(goToSection).offset().top,
-    }, 1500)
-})
+    const start = () => {
+        h1.style.height = (ulHeight / 2) + 'px';
+    }
+
+    start();
+
+    $('a.about').on('click', function () {
+        if (paragraph.className.includes('disapear')) {
+            $('header p').removeClass('disapear');
+            paragraph.textContent = "";
+            indexText = 0;
+            clearInterval(indexTyping);
+        }
+        $('header .about').addClass('disapear');
+        causeAddLetter();
+    })
+
+    $('nav a.main').on('click', function () {
+        if (aboutDiv.className.includes('disapear')) {
+            $('header .about').removeClass('disapear');
+            $('header p').addClass('disapear');
+        }
+    })
 
 
-$(".bootstrap-project").hover(function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.bootstrap').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
-}, function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.bootstrap').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
-});
-
-$(".sass-project").hover(function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.bootstrap').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.sass').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.gulp').addClass("no-filter");
-}, function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.bootstrap').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.sass').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.gulp').removeClass("no-filter");
-});
-
-$(".artwood-project").hover(function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.seo').addClass("no-filter");
-}, function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.seo').removeClass("no-filter");
-});
-
-$(".portfolio-project").hover(function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
-}, function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
-});
-
-$(".procural-project").hover(function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
-}, function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
-    $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
-});
+    $('a:not([data-type="about"])').on('click', function () {
+        const goToSection = "#" + $(this).attr('class');
+        $('body, html').animate({
+            scrollTop: $(goToSection).offset().top,
+        }, 1500)
+    })
 
 
-$(".sql-project").hover(function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.sql').addClass("no-filter");
-}, function () {
-    $(this).parent(".container-projects").siblings('.container-skills').children('.sql').removeClass("no-filter");
+    $(".bootstrap-project").hover(function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.bootstrap').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
+    }, function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.bootstrap').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
+    });
+
+    $(".sass-project").hover(function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.bootstrap').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.sass').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.gulp').addClass("no-filter");
+    }, function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.bootstrap').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.sass').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.gulp').removeClass("no-filter");
+    });
+
+    $(".artwood-project").hover(function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.seo').addClass("no-filter");
+    }, function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.seo').removeClass("no-filter");
+    });
+
+    $(".portfolio-project").hover(function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
+    }, function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.rwd').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
+    });
+
+    $(".procural-project").hover(function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').addClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').addClass("no-filter");
+    }, function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.html').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.css').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.jquery').removeClass("no-filter");
+        $(this).parent(".container-projects").siblings('.container-skills').children('.js').removeClass("no-filter");
+    });
+
+
+    $(".sql-project").hover(function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.sql').addClass("no-filter");
+    }, function () {
+        $(this).parent(".container-projects").siblings('.container-skills').children('.sql').removeClass("no-filter");
+    });
 });
